c7512e0d617bf316b6d4fddf048857c8ba6c1a4c
TigrexDai <wsgjsandrpk@foxmail.com>
2022-10-16 11:46:48 +0200

    finish cv2

--- base.py
+++ base.py
@@ -13,7 +13,7 @@ import joblib
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import cross_validate, KFold
+from sklearn.model_selection import cross_validate, KFold, StratifiedKFold, cross_val_score
 from sklearn.svm import LinearSVC, SVC
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import VotingClassifier
@@ -140,28 +140,25 @@ test_features_pca = pca.transform(test_features)
 all_features_pca = pca.transform(features)
 
 ## v1.2 DASK
-def daskrun(MLmethod):
+def daskrun(MLmethod, train, test):
     daskop = Client(n_workers=4)
     daskop
     with joblib.parallel_backend('dask'):
-        MLmethod.fit(train_features_pca, np.ravel(train_targets))
+        MLmethod.fit(train, np.ravel(test))
     print(MLmethod.score(test_features_pca, test_targets))
 
 ## v1.3.1 undersampling
 num_train_target = train_targets.sum()
-print('样本数:{}'.format(num_train_target))
+# print('Number of samples:{}'.format(num_train_target))
 
-# 减少第 0 类，直到第 1 类占总数的 10%
 undersample = RandomUnderSampler(sampling_strategy=0.2, random_state=42)
 train_features_pca_under, train_targets_under = undersample.fit_resample(train_features_pca, train_targets)
-print(train_features_pca_under.shape)
+# print(train_features_pca_under.shape)
 
 ## v1.3.2 oversampling
 oversample = SMOTE(sampling_strategy=0.2, random_state=42)
 train_features_pca_over, train_targets_over = oversample.fit_resample(train_features_pca, train_targets)
-print(train_features_pca_over.shape)
-
-
+# print(train_features_pca_over.shape)
 
 ## v2.0 models
 if __name__ == '__main__':
@@ -190,6 +187,7 @@ if __name__ == '__main__':
                                    min_samples_split=3,
                                    splitter='random',
                                    criterion='gini')
+    # daskrun(tempdt, train_features_pca_under, train_targets_under)
     # 0.4545057995634662
     # paras = {
     #     #'criterion': ('gini', 'entropy'),
@@ -209,25 +207,18 @@ if __name__ == '__main__':
     # print(DT.score(test_features_pca, test_targets))
 
     ###### KNN ######  0.9239
-    ############################################
+    knn = KNeighborsClassifier(n_neighbors=9, weights="uniform", p=4)
+
     # with joblib.parallel_backend('dask'):
-    #     # best_p = "" # 明科夫斯基的最佳参数
-    #     best_score = 0.0
-    #     best_k = -1
-    #     for k in range(1, 11):
-    #     # for p in range(1, 6):
-    #         knn_clf = KNeighborsClassifier(n_neighbors=k ,weights="uniform", p=4)
-    #         knn_clf.fit(train_features_pca, train_targets)
-    #         score = knn_clf.score(test_features_pca, test_targets)
-    #         if score > best_score:
-    #             # best_p = p
-    #             best_k = k
-    #             best_score = score
-    #     # print("best_p = ", best_p)
-    #     print("best_k = ", best_k)
-    #     print("best_score = ", best_score) # 如果过大或者过小就应该适当扩大k的范围
-    ####################################################
+    #     knn.fit(train_features_pca, train_targets)
+    # print(knn.score(test_features_pca, test_targets))
+
+    # [0.26989325 0.27036977 0.26851298 0.26828729 0.26899194]
+    # [0.27692137 0.27724928 0.28137828 0.28416321 0.27780335]
+
+    # Undersampling
     tempknn = KNeighborsClassifier(n_neighbors=3, weights="uniform", p=2)
+    # daskrun(tempknn, train_features_pca_under, train_targets_under)
     # [0.53387308 0.53406254 0.52939046 0.54362496 0.54450758] p=2
     # [0.33065327 0.32179226 0.314      0.28658848 0.29087158]
 
@@ -236,6 +227,7 @@ if __name__ == '__main__':
 
     # [0.53882688 0.53663178 0.53021348 0.5466031  0.53166667] p=6
     # [0.30463576 0.29417773 0.3047996  0.28006088 0.29028926]
+    ##############################GridSearch##############################
     # paras = {
     #     'n_neighbors': (list(range(1,11))),
     #     'p': list(range(1, 6))
@@ -245,15 +237,7 @@ if __name__ == '__main__':
     # best_dt = grid_dt.best_estimator_
     # print(best_dt)
     # print(grid_dt.best_score_)
-
-
-    knn = KNeighborsClassifier(n_neighbors=9 ,weights="uniform", p=4)
-
-    # [0.26989325 0.27036977 0.26851298 0.26828729 0.26899194]
-    # [0.27692137 0.27724928 0.28137828 0.28416321 0.27780335]
-    # with joblib.parallel_backend('dask'):
-    #     knn.fit(train_features_pca, train_targets)
-    # print(knn.score(test_features_pca, test_targets))
+    ######################################################################
 
     ###### Random Forest ###### 0.925 0.927
     RF = RandomForestClassifier(n_estimators=1500,
@@ -261,6 +245,9 @@ if __name__ == '__main__':
                                 bootstrap=False,
                                 max_depth=15,
                                 random_state=42)
+    # with joblib.parallel_backend('dask'):
+    #     RF.fit(train_features_pca_under, np.ravel(train_targets_under))
+    # print(RF.score(test_features_pca, test_targets))
 
     # [0.25114642 0.25111627 0.24963249 0.24856861 0.24938972] 10
     # [0.27670254 0.27184386 0.27900008 0.28094737 0.27671093]
@@ -268,10 +255,14 @@ if __name__ == '__main__':
     # [0.20205957 0.20190962 0.2007799  0.20149515 0.20059125] 15
     # [0.27262263 0.26848072 0.27506415 0.27703911 0.27407133]
 
-    # [0.69286853 0.69402137 0.69402299 0.69356498 0.69762149]
-    # [0.31414198 0.32309443 0.31486561 0.30346344 0.30803571]
+    # Undersampling
     temprf = RandomForestClassifier(n_estimators=1500,
                                     max_features='auto')
+
+    # [0.69286853 0.69402137 0.69402299 0.69356498 0.69762149]
+    # [0.31414198 0.32309443 0.31486561 0.30346344 0.30803571]
+
+    ##############################GridSearch##############################
     # paras = {
     #     # 'n_estimators': [100, 500, 1000, 1500],
     #     #'max_features': ['auto', 'sqrt', 'log2'],
@@ -285,10 +276,7 @@ if __name__ == '__main__':
     # best_rf = grid_rf.best_estimator_
     # print(best_rf)
     # print(grid_rf.best_score_)
-
-    # with joblib.parallel_backend('dask'):
-    #     RF.fit(train_features_pca_under, np.ravel(train_targets_under))
-    # print(RF.score(test_features_pca, test_targets))
+    ######################################################################
 
     ###### SVM ######
     def PolynomialSVC(degree, C=1.0):
@@ -303,40 +291,42 @@ if __name__ == '__main__':
     # [0.47889838 0.47153434 0.47305157 0.4833206  0.48824273]
     # [0.36951983 0.36752137 0.37423935 0.35592344 0.35905512]
     rbf_svc = SVC(kernel='rbf', gamma=0.1) # [0.1, 0.5, 1]
-    # daskrun(kernel_poly_svc)
+    # daskrun(kernel_poly_svc, train_features_pca_under, train_targets_under)
 
     ###### voting ######
     voting = VotingClassifier(estimators=[
-        ('RandomForest', RF),
+        ('DecisionTree', tempdt),
         ('SVM', SVC(kernel="poly", degree=3, C=1.0, probability=True)),
         ('KNN', tempknn)],
         voting='soft')
+
+    # daskrun(voting, train_features_pca_under, train_targets_under)
 
     # [0.54848065 0.53959018 0.54336441 0.5528209  0.5572002]
     # [0.31892195 0.30389908 0.30633609 0.2909699  0.30498866]
 
     # daskrun(voting)
 
-
-    cv = KFold(n_splits=5, shuffle=True, random_state=42)  # 实例化交叉验证方式
-    # 与sklearn中其他回归算法一样，随机森林的默认评估指标是R2，
-    # 但在机器学习竞赛、甚至实际使用时，我们很少使用损失以外的指标对回归类算法进行评估。对回归类算法而言，最常见的损失就是MSE。
-    result_f = cross_validate(rbf_svc,  # 要进行交叉验证的评估器
-                              all_features_pca, np.ravel(targets),  # 数据
-                              cv=cv,  # 交叉验证模式
-                              scoring="f1",  # 评估指标
-                              return_train_score=True,  # 是否返回训练分数
-                              verbose=True,  # 是否打印进程
-                              n_jobs=-1,  # 线程数
+## v3.0 Cross Validation
+    cv = KFold(n_splits=5, shuffle=True, random_state=42)
+    stracv = StratifiedKFold(n_splits=5)
+    result_f = cross_validate(voting,
+                              all_features_pca, np.ravel(targets),
+                              cv=stracv,
+                              scoring="f1",
+                              return_train_score=True,
+                              verbose=True,
+                              n_jobs=-1,
                               )
-    # trainRMSE_f = abs(result_f["train_score"]) ** 0.5
-    # testRMSE_f = abs(result_f["test_score"]) ** 0.5
-    # print(trainRMSE_f)
-    # print(testRMSE_f)
+    # # trainRMSE_f = abs(result_f["train_score"]) ** 0.5
+    # # testRMSE_f = abs(result_f["test_score"]) ** 0.5
+    # # print(trainRMSE_f)
+    # # print(testRMSE_f)
     trainf1 = result_f["train_score"]
     testf1 = result_f["test_score"]
     print(trainf1)
     print(testf1)
+
 
 
 
